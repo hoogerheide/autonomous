reduction fixes combining points; SBR calculation removed in favor of background measurement time penalty

reduction.py
@@ -37,7 +37,7 @@ def interpolate_background(Qbasis, backp: Union[ReflData, None] = None, backm: U
 
     return bkg.x, bkg.variance
 
-def DataPoint2ReflData(Qbasis, data: List[DataPoint], normbase='time') -> Tuple[Union[ReflData, None], Union[ReflData, None]]:
+def DataPoint2ReflData(Qbasis, data: List[DataPoint], normbase='none') -> Tuple[Union[ReflData, None], Union[ReflData, None]]:
     """
     Converts a list of DataPoint objects to tuple of numpy arrays (T, dT, L, dL, N, Ninc). Filters
     out any data points with zero incident intensity (protects against division by zero)
@@ -51,6 +51,7 @@ def DataPoint2ReflData(Qbasis, data: List[DataPoint], normbase='time') -> Tuple[
     """
 
     idata = {attr: [val for pt in data for val in getattr(pt, attr)] for attr in data_attributes}
+    idata['t'] = [pt.t for pt in data for _ in getattr(pt, 'T')] 
     idata['Ninc'] = np.round(idata['Ninc'])
     crit = idata['Ninc'] > 0
 
@@ -61,6 +62,7 @@ def DataPoint2ReflData(Qbasis, data: List[DataPoint], normbase='time') -> Tuple[
 
         q_edges = edges(Qbasis, extended=True)
         #argsort = np.argsort(idata['T'])
+        t = idata['t'][:, None, None]
         v = idata['N'][:, None, None]
         v += (v == 0)
         dv = np.sqrt(v)
@@ -73,7 +75,7 @@ def DataPoint2ReflData(Qbasis, data: List[DataPoint], normbase='time') -> Tuple[
                     sample=Sample(angle_x=T),
                     angular_resolution=dT,
                     detector=Detector(angle_x=2*T, wavelength=L, wavelength_resolution=wavelength_resolution),
-                    _v=v, _dv=dv, Qz_basis='detector', normbase=normbase)
+                    _v=v, _dv=dv, Qz_basis='actual', normbase=normbase)
 
         qz, dq, v, dv, _Ti, _dT, _L, _dL = _rebin_bank(data, 0, q_edges, 'poisson')
         data = QData(data, qz, dq, v, dv, _Ti, _dT, _L, _dL)
@@ -82,12 +84,12 @@ def DataPoint2ReflData(Qbasis, data: List[DataPoint], normbase='time') -> Tuple[
                         sample=Sample(angle_x=T),
                         angular_resolution=dT,
                         detector=Detector(angle_x=2*T, wavelength=L, wavelength_resolution=wavelength_resolution),
-                        _v=vinc, _dv=dvinc, Qz_basis='detector', normbase=normbase)
+                        _v=vinc, _dv=dvinc, Qz_basis='actual', normbase=normbase)
 
         qz, dq, v, dv, _Ti, _dT, _L, _dL = _rebin_bank(inc, 0, q_edges, 'poisson')
         inc = QData(data, qz, dq, v, dv, _Ti, _dT, _L, _dL)
 
-        apply_intensity_norm(data, inc)
+        apply_intensity_norm(data, inc, align_by='detector.angle_x')
 
         return data
     

simexp.py
@@ -261,7 +261,7 @@ class SimReflExperiment(object):
                             spec.detector.wavelength, spec.detector.wavelength_resolution,  \
                             spec.v, spec.dv, spec.Qz, spec.dQ
 
-            print(mT, mdT, mL, mdL, mR, mdR, mQ, mdQ)
+            #print(mT, mdT, mL, mdL, mR, mdR, mQ, mdQ)
 
             m.fitness.probe._set_TLR(mT, mdT, mL, mdL, mR, mdR, dQ=mdQ)
             m.fitness.probe.oversample(self.oversampling)
@@ -526,8 +526,10 @@ class SimReflExperiment(object):
 
             # calculate the existing background uncertainty. This is used to determine if additional
             # background measurements must be performed
+            specdata = [pt for step in self.steps for pt in step.points if (pt.model == mnum) & (pt.intent == Intent.spec)]
             bkgpdata = [pt for step in self.steps for pt in step.points if (pt.model == mnum) & (pt.intent == Intent.backp) & step.use]
             bkgmdata = [pt for step in self.steps for pt in step.points if (pt.model == mnum) & (pt.intent == Intent.backm) & step.use]
+            spec = DataPoint2ReflData(Qth, specdata)
             bkgp = DataPoint2ReflData(Qth, bkgpdata)
             bkgm = DataPoint2ReflData(Qth, bkgmdata)
             
@@ -540,19 +542,24 @@ class SimReflExperiment(object):
             # 1.0 so they will always be measured.
             dbkg = np.sqrt(bkgvar) if bkgvar is not None else np.full_like(Qth, 1.0)
 
-            print(qprof.shape, qbkg.shape)
-            plt.plot(Qth, qbkg)
-            plt.plot(Qth, qbkg + dbkg, Qth, qbkg - dbkg)
-            plt.plot(Qth, np.median(qprof, axis=0), Qth, np.median(qprof, axis=0) + np.std(qprof, axis=0),
-                    Qth, np.median(qprof, axis=0) - np.std(qprof, axis=0))
-            plt.yscale('log')
-            plt.show()
+            if False:
+                print(qprof.shape, qbkg.shape)
+                if spec is not None:
+                    plt.errorbar(spec.Qz, spec.v, spec.dv, fmt='.', capsize=4)
+                    spec = reduce(Qth, specdata, bkgpdata, bkgmdata)
+                    plt.errorbar(spec.Qz, spec.v, spec.dv, fmt='o', capsize=4)
+                plt.plot(Qth, qbkg)
+                plt.plot(Qth, qbkg + dbkg, Qth, qbkg - dbkg)
+                plt.plot(Qth, np.median(qprof, axis=0), Qth, np.median(qprof, axis=0) + np.std(qprof, axis=0),
+                        Qth, np.median(qprof, axis=0) - np.std(qprof, axis=0))
+                plt.yscale('log')
+                plt.show()
 
             # define signal to background. For now, this is just a scaling factor on the effective rate
             # reference: Hoogerheide et al. J Appl. Cryst. 2022
             sbr = qprof / qbkg
-            refl = qprof/(1+2/sbr)
-            refl = np.clip(refl, a_min=0, a_max=None)
+            #refl = qprof/(1+2/sbr)
+            refl = np.clip(qprof, a_min=0, a_max=None)
             
             # perform interpolation. xqprof should have shape N x XD. This is a slow step (and should only be done once)
             interp_refl = interp1d(Qth, refl, axis=1, fill_value=(refl[:,0], refl[:,-1]), bounds_error=False)
@@ -646,6 +653,16 @@ class SimReflExperiment(object):
                 # Compare uncertainty in background to expected uncertainty in reflectivity
                 t_0 = 1.0 / (incident_neutrons * xqbkg * (xdbkg / xqbkg) ** 2)
                 t_bkg = 1.0 / (incident_neutrons * xqbkg * (meas_sigma / xqbkg) ** 2)
+                if False:
+                    plt.plot(t_0)
+                    plt.plot(t_bkg)
+                    plt.yscale('log')
+                    plt.show()
+
+                    #plt.plot(xdbkg)
+                    #plt.plot(meas_sigma)
+                    #plt.yscale('log')
+                    #plt.show()
 
                 # Calculate time required to achieve target background; negative times indicate
                 # that background measurement is not necessary